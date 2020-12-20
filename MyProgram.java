faac827afc3ef975e9299a87195ad53533d39856
Lian <55862116+liankang214@users.noreply.github.com>
2020-12-20 14:49:43 -0500

    Add files via upload

---
+++ MyProgram.java
@@ -0,0 +1,8 @@
+public class MyProgram {
+  public static void main(String[] args){
+    User user = new User();
+    user.firstName = "Lian";
+    user.lastName = "Kang";
+    System.out.println(user.getFullName());
+  }
+}
