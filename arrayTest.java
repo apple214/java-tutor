9c40c58620f47fae8d7d6318ef4a279ea25b00f5
Lian <55862116+liankang214@users.noreply.github.com>
2020-12-27 10:46:58 -0500

    Add files via upload

---
+++ arrayTest.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class arrayTest {
+  public static void main(String[] args){
+
+    Scanner scanner = new Scanner(System.in);
+    int size = scanner.nextInt();
+    int[] ages = new int[size];
+    //int[] ages = {10,9,8};
+    ages[1] = 200;
+    System.out.println(ages[0]);
+
+    try{
+      System.out.println(ages[3]);
+    } catch (
+        Exception e
+    ){
+      System.out.println(e);
+    }
+
+
+    for(int i=0; i<ages.length; i++){
+      int x = scanner.nextInt();
+      ages[i] = x;
+
+      if(ages[i] == 20){
+        System.out.println("Found at index" + i);
+      }
+    }
+
+    Arrays.sort(ages);
+    Arrays.parallelSort(ages); //大数组适用
+    System.out.println(Arrays.toString(ages));
+
+    int ages2[] = {1,2,3};
+    if(ages.equals(ages2)){
+      System.out.println("bingo");
+    }
+
+    if(Arrays.equals(ages,ages2)){
+      System.out.println("finally");
+    };
+
+    Arrays.fill(ages, 100); //填充
+
+    scanner.close();
+  }
+}
