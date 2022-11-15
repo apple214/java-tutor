1088762823e5e6f6abb7575471ff9b147ffe74ce
Lian <55862116+liankang214@users.noreply.github.com>
2022-11-15 10:45:12 -0500

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -3,18 +3,15 @@ import java.util.Scanner;
 public class helloWorld {
 
   public static void main(String[] args) {
-    //请求输入
+
     String askInput = new String("Enter the name ('end' to exit)");
     System.out.println(askInput);
-
-    //输入名字
+    
     Scanner input = new Scanner(System.in);
     String name = input.nextLine();
 
-    //输出你好
     System.out.println("Hello " + name + "!");
 
-    //判断是否为中止指令
     String end = new String("end");
     while(!name.equals(end)){
       System.out.println(askInput);
@@ -22,7 +19,6 @@ public class helloWorld {
       System.out.println("Hello " + name + "!");
     }
 
-    //好习惯
     input.close();
   }
 }
