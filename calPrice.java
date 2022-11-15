c2713f9c6632270c73b2ca4f7214f3ca243aebf0
Lian <55862116+liankang214@users.noreply.github.com>
2022-11-15 10:44:08 -0500

    Update calPrice.java

--- calPrice.java
+++ calPrice.java
@@ -4,7 +4,6 @@ public class calPrice {
   public static void main(String[] args){
     System.out.print("How many grams does the mail weigh? ");
 
-    //输入克数
     Scanner input = new Scanner(System.in);
     int weight = input.nextInt();
 
@@ -14,26 +13,24 @@ public class calPrice {
       cost = 4;
     }
     if(weight > 30 &&  weight <= 50){
-      cost = 5.5f; //double不需要加f
+      cost = 5.5f; //no f if double
     }
     if(weight > 50 &&  weight <= 100){
       cost = 7;
     }
 
-    //超重
+    //over weight
     if(weight > 100){
       cost = 7;
       int rest = weight - 100;
       for(rest = rest; rest > 0; rest = rest - 50){
         cost += 2.50;
       }
-
     }
 
-    //输出
     System.out.print("The cost to mail this letter is $" + cost);
 
-    //好习惯
+    //good habit
     input.close();
   }
 }
